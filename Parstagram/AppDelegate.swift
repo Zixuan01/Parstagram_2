8f63140f099b544f536f12f1e9ce881633aa0bd7
Zixuan Mao <zixuanmao@ucsb.edu>
2020-12-04 16:06:20 -0800

    Final for the Parstagram

---
+++ Parstagram/AppDelegate.swift
@@ -0,0 +1,42 @@
+//
+//  AppDelegate.swift
+//  Parstagram
+//
+//  Created by Hedda on 11/20/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import Parse
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+    let parseConfig = ParseClientConfiguration {
+            $0.applicationId = "aOQtWj30F6J4Bpuf4J2U7Twre74YDZAtqvapDvA9"
+            $0.clientKey = "w6734o7frVdYcv8CNfBryDffXJxh1Z2EB17oOR0p"
+            $0.server = "https://parseapi.back4app.com"
+    }
+    Parse.initialize(with: parseConfig)
+    return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'Parstagram' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  # This pulls in the latest version of Alamofire >= 4.4 but less than 5.0.
+  # Pods for Parstagram
+  pod 'Parse'
+  pod 'AlamofireImage'
+  pod 'MessageInputBar'
+  # Add additional dependencies
+  
+  target 'ParstagramTests' do
+    inherit! :search_paths
+  end
+  
+  target 'ParstagramUITests' do
+  end
+  
+end
